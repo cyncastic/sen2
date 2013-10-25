8c712d593e456e19ec8ea242eac9be4fed9db257
sedicted <john@cyncastic.local>
2013-10-24 22:14:49 -0400

    first working header changer

--- app/models/header.rb
+++ app/models/header.rb
@@ -1,5 +1,24 @@
 class Header < ActiveRecord::Base
-  validates :image, presence: true
 
   mount_uploader :image, HeaderUploader
+
+  validates :image, presence: true
+
+  validate :only_one_active_header
+  scope :active, where(:active => true)
+
+  protected
+
+  def only_one_active_header
+    return unless active?
+
+    matches = Header.active
+    if persisted?
+      matches = matches.where('id != ?', id)
+    end
+    if matches.exists?
+      errors.add(:active, 'cannot have another active header')
+    end
+  end
+
 end
